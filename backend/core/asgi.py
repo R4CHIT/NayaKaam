ddec347ff9c2ad381a057de7f66bd45c61f5053e
R4CHIT <tiwarirachit50@gmail.com>
2025-12-15 17:58:23 +0545

    refactor: Clean up asgi.py by organizing imports and improving formatting

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -1,18 +1,24 @@
 # core/asgi.py
 import os
+import django
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
+django.setup()
+
 import notification.routing
 import message.routing
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket":
-        AuthMiddlewareStack(
-            URLRouter(notification.routing.websocket_urlpatterns + message.routing.websocket_urlpatterns)
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            notification.routing.websocket_urlpatterns
+            + message.routing.websocket_urlpatterns
         )
-    
+    ),
 })
