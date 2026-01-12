6fee174764e9552153a380b06b49bec8f7d28e87
R4CHIT <tiwarirachit50@gmail.com>
2026-01-12 23:54:16 +0545

    refactor: Update axios baseURL to production endpoint

--- frontend/src/axios.jsx
+++ frontend/src/axios.jsx
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://nayakaam.onrender.com/",
-  baseURL:"http://127.0.0.1:8000/",
+  baseURL: "https://nayakaam.onrender.com/",
+  // baseURL:"http://127.0.0.1:8000/",
 });
 
 const refreshInstance = axios.create({
-  // baseURL: "https://nayakaam.onrender.com/",
-  baseURL:"http://127.0.0.1:8000/",
+  baseURL: "https://nayakaam.onrender.com/",
+  // baseURL:"http://127.0.0.1:8000/",
 });
 
 instance.interceptors.response.use(
